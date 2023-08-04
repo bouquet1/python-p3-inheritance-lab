1c1357821e47671dfa20d097275bd1a7942326ca
bouquet1 <bukuleta@gmail.com>
2023-08-04 15:45:19 -0700

    teacher.py passed

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,23 @@ from user import User
 import random
 
 class Teacher(User):
+    #super() function to call the constructor (test says positional arguments) of the base class.
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge =[
+            "programming is hard, but it's worth it",
+            "Python function call definition",
+            "object-oriented teacher instance",
+        ]
 
     def teach(self):
-        pass
+       random_knowledge = random.choice(self.knowledge)
+       return random_knowledge
+    
+    #test calls for random.randint but random.choice worked. here is alternative with it.
+    # def teach(self):
+        # random_index = random.randint(0, len(self.knowledge) - 1)
+        # random_knowledge = self.knowledge[random_index]
+        # return random_knowledge
+
+
