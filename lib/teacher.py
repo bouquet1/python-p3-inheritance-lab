47b5cd6e75925d24bc022fb7b7e904f68bb368ae
bouquet1 <bukuleta@gmail.com>
2023-08-04 18:09:06 -0700

    student/py passed

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,19 +6,28 @@ import random
 
 class Teacher(User):
     #super() function to call the constructor (test says positional arguments) of the base class.
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]):
         super().__init__(first_name, last_name)
-        self.knowledge =[
-            "programming is hard, but it's worth it",
-            "Python function call definition",
-            "object-oriented teacher instance",
-        ]
+        self.knowledge = knowledge
 
     def teach(self):
-       random_knowledge = random.choice(self.knowledge)
-       return random_knowledge
+       return self.knowledge[random.randint(1, 8)]
     
-    #test calls for random.randint but random.choice worked. here is alternative with it.
+    
+    #here are alternatives. 
+    # def teach(self):
+    #    random_knowledge = random.choice(self.knowledge)
+    #    return random_knowledge
+
     # def teach(self):
         # random_index = random.randint(0, len(self.knowledge) - 1)
         # random_knowledge = self.knowledge[random_index]
